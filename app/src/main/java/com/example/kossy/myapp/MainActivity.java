62ac15200b755f2477e3343b99348c50ac1eaaaf
kossy1 <sunnygrin007@gmail.com>
2019-02-18 15:48:28 +0100

    my

---
+++ app/src/main/java/com/example/kossy/myapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.kossy.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+
+        Log.i("info", myTextField.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
